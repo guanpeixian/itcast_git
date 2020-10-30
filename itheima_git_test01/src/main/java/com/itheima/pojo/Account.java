94146d1a75cf6f3dc3a3dc25b3a19718b16ce9e7
peson <guanpx135@163.com>
2020-10-30 21:09:46 +0800

    firstcommit

---
+++ itheima_git_test01/src/main/java/com/itheima/pojo/Account.java
@@ -0,0 +1,45 @@
+package com.itheima.pojo;
+
+/**
+ * @author guanpx
+ * @create 2020/10/29 17:59
+ * @description
+ */
+public class Account {
+    private Integer id;
+    private String name;
+    private Double money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
